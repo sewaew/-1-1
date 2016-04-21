f4cbe95418800cff3cf2c48e865792a51b0bea39
Evegenii Sevastjanov <sewaew@mail.ru>
2016-04-21 18:08:05 +0600

    Причесываем

--- fail.py
+++ fail.py
@@ -9,20 +9,19 @@ print ("В файле query1 выбрать все строки, в которы
 print;
 
 
-#найдены сопоставления
-
+#чистим файд text.txt от предыдущих тестов
 f1 = open('text.txt', 'w')
 f1.write
 f1.close()
 
 
-f = open('query1')
-for line in f:
-   match = re.search(r'^.....R.*', line)
-   if match:
-      print(match.group())
-      a = match.group()
-      f1 = open('text.txt', 'a')
-      f1.write(a + '\n')
-      f1.close()
+f = open('query1')							#открываем файл для чтения
+for line in f:								#организовавыем масси для чтения файла по строке
+   match = re.search(r'^.....R.*', line)	#ищим совпадения по шаблону
+   if match:								#если совпадания найдены
+      print(match.group())					#печатаем совпадения
+      a = match.group()						#переменной а присваем совпадшую строку
+      f1 = open('text.txt', 'a')			#открываем файл для записи
+      f1.write(a + '\n')					#записываем совпадения в файл
+      f1.close()							#закрываем файл
 f.close()
